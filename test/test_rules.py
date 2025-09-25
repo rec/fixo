3b3a746241ec276c5e1e75dd251615af11c108a8
Tom Ritchford <tom@swirly.com>
2025-09-25 12:35:52 +0200

    Add test/rules.json

--- test/test_rules.py
+++ test/test_rules.py
@@ -1,7 +1,17 @@
+import json
+from pathlib import Path
 from typing import Protocol
 
 from fixo.rule import Rule
 
+DATA = Path(__file__).parent / "rules.json"
+
 
 def test_simple_rule_from_data():
     Rule.create(parent=".pyright")
+
+
+def test_rules_from_file():
+    rule_data = json.loads(DATA.read_text())
+
+    rules = [Rule.create(**r) for r in rule_data.values()]
