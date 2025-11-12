7c8d4541e6d788deab4ddebf85af30733e4d35c8
Tom Ritchford <tom@swirly.com>
2025-11-12 12:14:08 +0100

    Extract code into fixo/rules/rules.py

---
+++ fixo/rules/rules.py
@@ -0,0 +1,27 @@
+import json
+from functools import cache
+from pathlib import Path
+
+from ..rule import Rule
+
+
+def make_rules(s: str) -> dict[str, Rule]:
+    return Rule.create_all(
+        json.loads(s if "{" in s else Path(s).read_text()) if s else _RULE_DATA
+    )
+
+
+@cache
+def default_rules() -> dict[str, Rule]:
+    return Rule.create_all(_RULE_DATA)
+
+
+_RULE_DATA = {
+    "bools": {"parent": ".pyright", "name_match": "(is|has)_.*", "type_name": "bool"},
+    "self_params": {
+        "parent": ".pyright",
+        "categories": ["function"],
+        "name_match": "self",
+        "type_name": "torch.Tensor",
+    },
+}
