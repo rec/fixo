b775dd76b1145cf1867faa652647756d42bb892a
Tom Ritchford <tom@swirly.com>
2025-12-30 18:12:29 +0100

    Make args a cached global in __main__

--- fixo/__main__.py
+++ fixo/__main__.py
@@ -6,7 +6,7 @@ import shlex
 import subprocess
 import sys
 from collections.abc import Sequence
-from functools import cached_property, partial
+from functools import cache, cached_property, partial
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
@@ -23,6 +23,36 @@ MAX_ERROR_CHARS = 1024
 _err = partial(print, file=sys.stderr)
 
 
+@cache
+def args() -> Namespace:
+    parser = argparse.ArgumentParser()
+    add = parser.add_argument
+
+    help = """If the files end in .json, they are edits to be executed, otherwise,
+    they are a list of files or directories to be passed to the type checker."""
+    add("files", nargs="+", type=Path, help=help)
+
+    help = "Immediately edit, don't write an edit file to be executed"
+    add("-i", "--edit-immediately", action="store_true", help=help)
+
+    help = "Command line or JSON file for type completeness"
+    add("-c", "--type-completeness", type=str, default="", help=help)
+
+    help = "Rules from the rule set to use"
+    add("-r", "--rules", nargs="*", help=help)
+
+    help = "The rule set to use"
+    add("-s", "--rule-set", type=str, default="", help=help)
+
+    help = "Which type checker to use?"
+    add("-t", "--type-checker", default="pyright", help=help)
+
+    help = "Print more debug info"
+    add("-v", "--verbose", action="store_true", help=help)
+
+    return parser.parse_args()
+
+
 class FixoError(Exception):
     pass
 
@@ -36,43 +66,43 @@ def main() -> None:
 
 class Fixo:
     def main(self) -> None:
-        if not any(f.suffix == ".json" for f in self.args.files):
+        if not any(f.suffix == ".json" for f in args().files):
             self._find()
-        elif len(self.args.files) == 1:
+        elif len(args().files) == 1:
             self._execute()
         else:
             raise FixoError("Only only .json file is allowed")
 
     @cached_property
     def rules(self) -> dict[str, Rule]:
-        rules = make_rules(self.args.rule_set)
-        if not self.args.rules:
+        rules = make_rules(args().rule_set)
+        if not args().rules:
             return rules
 
-        if bad := ", ".join(r for r in self.args.rules if r not in rules):
+        if bad := ", ".join(r for r in args().rules if r not in rules):
             raise FixoError(f"Unknown --rule: {bad}")
 
-        return {r: rules[r] for r in self.args.rules}
+        return {r: rules[r] for r in args().rules}
 
     def _execute(self) -> None:
-        (file,) = self.args.files
+        (file,) = args().files
         data = json.loads(file.read_text())
         edits = {k: [type_edit.TypeEdit(**i) for i in v] for k, v in data.items()}
         self._edit(edits)
 
     def _find(self) -> None:
-        tc = self.args.type_completeness
-        if (p := Path(tc)).exists():
-            assert p.suffix == ".json"
+        tc = args().type_completeness  # or
+
+        if (p := Path(tc)).exists() and p.suffix == ".json":
             results = p.read_text()
         else:
-            results = self.run((*shlex.split(tc), *self.args.files))
+            results = self.run((*shlex.split(tc), *args().files))
 
         first_rule = next(iter(self.rules.values()))
         file_messages = first_rule.file_messages(results)
         edits = {k: list(v.edits(file_messages)) for k, v in self.rules.items()}
 
-        if self.args.edit_immediately:
+        if args().edit_immediately:
             self._edit(edits)
         else:
             edits_json = {k: [i.asdict() for i in v] for k, v in edits.items()}
@@ -87,47 +117,16 @@ class Fixo:
                 p.write_text(type_edit.perform_type_edits(edits, PythonFile(path=p)))
             except Exception as e:
                 _err(f"ERROR: {p}:", *e.args)
-                if self.args.verbose:
+                if args().verbose:
                     import traceback
 
                     traceback.print_exc()
             else:
                 _err(f"{p}: {len(edits)}")
 
-    @cached_property
-    def args(self) -> argparse.Namespace:
-        parser = argparse.ArgumentParser()
-
-        help = """If the files end in .json, they are edits to be executed, otherwise,
-        they are a list of files or directories to be passed to the type checker."""
-        parser.add_argument("files", nargs="+", type=Path, help=help)
-
-        help = "Immediately edit, don't write an edit file to be executed"
-        parser.add_argument("-i", "--edit-immediately", action="store_true", help=help)
-
-        help = "Rules from the rule set to use"
-        parser.add_argument("-r", "---rules", nargs="*", help=help)
-
-        help = "The rule set to use"
-        parser.add_argument("-s", "---rule-set", type=str, default="", help=help)
-
-        help = "Command line or JSON file for type completeness"
-        parser.add_argument(
-            "-t",
-            "--type-completeness",
-            type=str,
-            default="",
-            help=help,
-        )
-
-        help = "Print more debug info"
-        parser.add_argument("-v", "--verbose", action="store_true", help=help)
-
-        return parser.parse_args()
-
     def run(self, cmd: str | Sequence[str], check: bool = True, **kwargs: Any) -> str:
         """Run a subprocess and return stdout as a string"""
-        if self.args.verbose:
+        if args().verbose:
             print("$", *([cmd] if isinstance(cmd, str) else cmd))
 
         shell = kwargs.get("shell", False)
@@ -138,7 +137,7 @@ class Fixo:
         assert shell == isinstance(cmd, str)
         p = subprocess.run(cmd, text=True, capture_output=True, **kwargs)
 
-        if self.args.verbose or (check and p.returncode):
+        if args().verbose or (check and p.returncode):
             # TODO: Fix this output for lintrunner mode
             print(p.stdout[:MAX_ERROR_CHARS], file=sys.stderr)
             error = p.stderr if p.returncode else p.stderr[:MAX_ERROR_CHARS]
